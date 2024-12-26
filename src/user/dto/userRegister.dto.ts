4cc87cdfc6f08bf58edf063d61c037487ea0ecf9
kasun <88395585+ka0un@users.noreply.github.com>
2024-12-27 00:39:18 +0530

    [feature] added simple apis and service methods for user functionality

---
+++ src/user/dto/userRegister.dto.ts
@@ -0,0 +1,5 @@
+export class UserRegisterDto {
+  readonly name: string;
+  readonly email: string;
+  readonly password: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schemas/user.schema';
 
@@ -9,5 +9,23 @@ export class UserController {
   @Get('all')
   async getAllUsers(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  //get one user
+  @Get('')
+  async getUser(uuid: string): Promise<User> {
+    return this.userService.findById(uuid);
+  }
+
+  //register user
+  @Post('register')
+  async registerUser(@Body() user: User): Promise<User> {
+    return this.userService.createUser(user);
+  }
+
+  //login
+  @Post('login')
+  async loginUser(@Body() user: User): Promise<boolean> {
+    return this.userService.validateUser(user.email, user.password);
   }
 }
