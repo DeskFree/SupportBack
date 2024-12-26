f2528b3bc8a5ec63857c97c7ad1ef207ead4782b
kasun <88395585+ka0un@users.noreply.github.com>
2024-12-26 12:35:41 +0530

    int

---
+++ commands.md
@@ -0,0 +1,11 @@
+## Create a Module
+```nest g module user```
+
+## Create a Controller
+```nest g controller user```
+
+## Create a Service
+```nest g service user```
+
+## Install Mongoose and Config
+```npm i mongoose @nestjs/mongoose @nestjs/config -- save ```
--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,8 +1,30 @@
-import { Schema } from "@nestjs/mongoose";
-
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({
-  timestamps: true
+  timestamps: true,
 })
-export class Book {
-  title: string;
+export class User {
+  @Prop()
+  @Prop({ unique: true })
+  _id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  status: string;
+
+  @Prop()
+  avatar: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
