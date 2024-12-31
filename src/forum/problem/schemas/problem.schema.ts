ea3eaadb2536cf86026d0dd4980e60b4be99e7a8
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2024-12-31 20:35:12 +0530

    Combining Class and Mongoose Features

--- src/forum/problem/schemas/problem.schema.ts
+++ src/forum/problem/schemas/problem.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 import { ProblemStatus } from '../enums/status.enum';
 
 export type ProblemDocument = Problem & Document;
