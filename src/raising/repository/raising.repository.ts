df4b938a4ea8b71ff7bbebdc98f0d3f1d2b1a37f
Nethmi <nethmi.h@olak.org>
2025-01-12 23:35:49 +0530

    fix:update desc:: fix ticket Raising

--- src/raising/repository/raising.repository.ts
+++ src/raising/repository/raising.repository.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Raising } from '../schemas/raising.schema';
 import { CreateRaisingDto } from '../dto/create-raising.dto';
 import { UpdateRaisingDto } from '../dto/update-raising.dto';
-
 
 @Injectable()
 export class RaisingRepository {
