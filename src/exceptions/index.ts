54a79c06a2b1c7c8ae53f02780d5f00065a0a15d
ChamithDilshan <61306553+DilZhaan@users.noreply.github.com>
2025-01-15 12:47:37 +0530

    feat: add exception and filter exports for improved error handling and validation

---
+++ src/exceptions/index.ts
@@ -0,0 +1,6 @@
+export * from './database-rollback.exception';
+export * from './database-rollback.exception';
+export * from './duplicate-problem.exception';
+export * from './log-failure.exception';
+export * from './too-many-requests-exception';
+export * from './unauthorized-access.exception';
---
+++ src/forum/pipes/index.ts
@@ -0,0 +1,2 @@
+export * from './id-string-to-obj-converter.pipe';
+export * from './problem-validator.pipe';
