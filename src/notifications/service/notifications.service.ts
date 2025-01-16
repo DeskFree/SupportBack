177e259aa7034d67213ae48da8a685d6f34238ea
Minidu Thiranjaya <74867152+MiniduTH@users.noreply.github.com>
2025-01-16 15:38:17 +0530

    feat: enhance notification system with user email handling and update DTOs

--- src/notifications/service/notifications.service.ts
+++ src/notifications/service/notifications.service.ts
@@ -1,10 +1,11 @@
 // src/notifications/services/notification.service.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateNotificationDto } from '../dto/notificationCreateDto';
 import { NotificationRepository } from '../repository/notification.repository';
 import { EmailService } from 'src/email/email.service';
 import { UserRole } from '../enum/notifications.enum';
 import { UserService } from 'src/user/user.service';
+import { UpdateNotificationDto } from '../dto/notificationUpdate.dto';
 
 @Injectable()
 export class NotificationService {
@@ -19,18 +20,26 @@ export class NotificationService {
       createNotificationDto,
     );
 
-    if (createNotificationDto.userId) {
-      // Send an email notification
+    let emails: string[] = ['minidu@Mail.com'];
+    console.log(createNotificationDto.userIds)
+    createNotificationDto.userIds.forEach(async (id) => {
+      if (id) {
+        // Fetch email for a single user
+        const user = await this.userService.findById(id);
+        if (user && user.email) {
+          emails.push(user.email);
+        }
+      }
+    });
+
+    if (emails.length > 0) {
       const emailSubject = createNotificationDto.title;
       const emailText = createNotificationDto.message;
-      const receiver = this.userService.findById(createNotificationDto.userId);
-      const receiverEmail = (await receiver).email;
-
       await this.emailService.sendEmail(
-        receiverEmail,
+        emails, // Array of email addresses
         emailSubject,
         emailText,
-      ); // Replace with the user's email
+      );
     }
 
     return notification;
@@ -40,11 +49,26 @@ export class NotificationService {
     return this.notificationRepository.findAllByUserIdAndRole(userId, userRole);
   }
 
-  async markNotificationAsRead(notificationId: string) {
-    return this.notificationRepository.markAsRead(notificationId);
+  async markNotificationAsRead(notificationId: string, userId: string) {
+    return this.notificationRepository.markAsRead(notificationId,userId);
   }
 
   async readAllNotifications() {
-    return await this.notificationRepository.read();
+    return  await this.notificationRepository.read();
+  }
+
+  async updateNotification(notificationId: string, updateNotificationDto: UpdateNotificationDto) {
+    const updatedNotification = await this.notificationRepository.update(
+      notificationId,
+      updateNotificationDto,
+    );
+    if (!updatedNotification) {
+      throw new NotFoundException('Notification not found');
+    }
+    return updatedNotification;
+  }
+
+  async deleteNotification(notificationId: string) {
+    return this.notificationRepository.delete(notificationId);
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,6 +8,6 @@ import { UserSchema } from './schemas/user.schema';
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserModule],
+  exports: [UserService],
 })
 export class UserModule {}
